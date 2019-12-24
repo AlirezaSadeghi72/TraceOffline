78523b55aaae4a2320f51e4bbbd7a7f8eee872cb
Alireza Sadeghi <dev.Alirezasadeghi@gmail.com>
2019-12-24 08:59:04 +0330

    Add project files.

---
+++ Atiran.Rahyab/Base/TraceOffline.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atiran.Rahyab.Base
+{
+    public class TraceOffline : UserControl
+    {
+        public TraceOffline()
+        {
+            InitializeComponent();
+        }
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TraceOffline
+            // 
+            this.Name = "TraceOffline";
+            this.Size = new System.Drawing.Size(1143, 714);
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ Atiran.Rahyab/Default.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atiran.Rahyab
+{
+    public class Default:Form
+    {
+        public Default()
+        {
+            InitializeComponent();
+        }
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Default
+            // 
+            this.ClientSize = new System.Drawing.Size(883, 457);
+            this.Name = "Default";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Default_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void Default_Load(object sender, EventArgs e)
+        {
+            var offline = new Base.TraceOffline();
+            offline.Dock = DockStyle.Fill;
+            this.Controls.Add(offline);
+        }
+    }
+}
